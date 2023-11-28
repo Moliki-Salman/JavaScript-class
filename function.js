226bd29512703307cde18622076f3903f4d936c6
Moliki-Salman <salmanameenat@gmail.com>
2023-11-28 11:59:22 +0100

    parametised function

---
+++ function.js
@@ -0,0 +1,11 @@
+function fnc () {
+let z = x + y;
+return z
+}
+
+// parameterised function: that allows the acceptance of value, they enable you to pass values into the function.
+fn = (c, d) => {
+let z = c *5 + 4
+return z
+}
+console.log(fn(10, 20))
