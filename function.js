e5b534f2791f23ae75618d93b335b356ff0bb61e
Moliki-Salman <salmanameenat@gmail.com>
2023-11-28 12:06:29 +0100

    understanding global scope in functions

--- function.js
+++ function.js
@@ -1,11 +1,17 @@
-function fnc () {
-let z = x + y;
-return z
-}
+let x = 50; //global scope
+let y = 3;
+let ab = 0;
+fun = () => {
+  let z = x + y;
+  return z;
+};
+console.log(fun());
+ab = fun();
+console.log(ab);
 
-// parameterised function: that allows the acceptance of value, they enable you to pass values into the function.
+// parameterised function: allows the acceptance of value, they enable you to pass values into the function.
 fn = (c, d) => {
-let z = c *5 + 4
-return z
-}
-console.log(fn(10, 20))
+  let z = c * 5 + 4;
+  return z;
+};
+console.log(fn(10, 20));
